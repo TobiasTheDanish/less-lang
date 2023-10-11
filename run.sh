b6a59723804bc781381ea059dababbabe5cdc104
TobiasTheDanish <t.hc199811@gmail.com>
2023-10-12 00:45:03 +0200

    Minor changes to run script, for better error messages.

--- run.sh
+++ run.sh
@@ -2,10 +2,18 @@
 
 echo "./less $2 -o $1"
 ./less $2 -o $1
+retval=$?
+if [ $retval -ne 0 ] ; then
+	echo "Compilation exited with non zero exit code: '$retval'"
+	exit $retval
+fi
 
 echo "nasm -felf64 -g $1.asm"
-if ! nasm -felf64 -g $1.asm; then
-	exit $?
+nasm -felf64 -g $1.asm
+retval=$?
+if [ $retval -ne 0 ] ; then
+	echo "Assembler exited with non zero exit code: '$retval'"
+	exit $retval
 fi
 
 echo "ld -g -o $1 $1.o"
