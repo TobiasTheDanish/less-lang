9163bdc19580d7387f8d54eb4a3a7ba52d747f23
THCSC <tobias@skancode.dk>
2024-08-01 21:46:13 +0200

    formatting

--- src/lexer.c
+++ src/lexer.c
@@ -6,343 +6,337 @@
 #include <stdlib.h>
 #include <string.h>
 
-lexer_T* lexer_from_file(char* filePath){
-	file_T* file = open_file(filePath);
-	char* content = read_file(file);
+lexer_T *lexer_from_file(char *filePath) {
+  file_T *file = open_file(filePath);
+  char *content = read_file(file);
 
-	lexer_T* lexer = malloc(sizeof(lexer_T));
-	lexer->content = content;
-	lexer->i = 0;
-	lexer->c = content[0];
-	lexer->loc = calloc(1, sizeof(location_T));
-	lexer->loc->filePath = filePath;
-	lexer->loc->col = 1;
-	lexer->loc->row = 1;
+  lexer_T *lexer = malloc(sizeof(lexer_T));
+  lexer->content = content;
+  lexer->i = 0;
+  lexer->c = content[0];
+  lexer->loc = calloc(1, sizeof(location_T));
+  lexer->loc->filePath = filePath;
+  lexer->loc->col = 1;
+  lexer->loc->row = 1;
 
-	return lexer;
+  return lexer;
 }
 
-lexer_T* lexer_from_string(char* content) {
-	lexer_T* lexer = malloc(sizeof(lexer_T));
-	lexer->content = content;
-	lexer->i = 0;
-	lexer->c = content[0];
-	lexer->loc = calloc(1, sizeof(location_T));
-	lexer->loc->filePath = "String literal";
+lexer_T *lexer_from_string(char *content) {
+  lexer_T *lexer = malloc(sizeof(lexer_T));
+  lexer->content = content;
+  lexer->i = 0;
+  lexer->c = content[0];
+  lexer->loc = calloc(1, sizeof(location_T));
+  lexer->loc->filePath = "String literal";
 
-	return lexer;
+  return lexer;
 }
 
-void advance(lexer_T* lexer) {
-	if (lexer->c == '\n') {
-		lexer->loc->col = 1;
-		lexer->loc->row += 1;
-	} else {
-		lexer->loc->col += 1;
-	}
+void advance(lexer_T *lexer) {
+  if (lexer->c == '\n') {
+    lexer->loc->col = 1;
+    lexer->loc->row += 1;
+  } else {
+    lexer->loc->col += 1;
+  }
 
-	lexer->i += 1;
-	lexer->c = lexer->content[lexer->i];
+  lexer->i += 1;
+  lexer->c = lexer->content[lexer->i];
 }
 
-token_T* read_ident(lexer_T* lexer, location_T* loc) {
-	char* value = calloc(2, sizeof(char));
-	size_t i = 0;
+token_T *read_ident(lexer_T *lexer, location_T *loc) {
+  char *value = calloc(2, sizeof(char));
+  size_t i = 0;
 
-	do {
-		value[i++] = lexer->c;
+  do {
+    value[i++] = lexer->c;
 
-		value = realloc(value, (i + 1) * sizeof(char));
-		advance(lexer);
-	} while (isalnum(lexer->c) || lexer->c == '_');
+    value = realloc(value, (i + 1) * sizeof(char));
+    advance(lexer);
+  } while (isalnum(lexer->c) || lexer->c == '_');
 
-	if (strcmp("else", value) == 0) {
-		return token_new(T_ELSE, value, loc);
-	} else if (strcmp("dump", value) == 0) {
-		return token_new(T_DUMP, value, loc);
-	} else if (strcmp("mut", value) == 0) {
-		return token_new(T_MUT, value, loc);
-	} else if (strcmp("let", value) == 0) {
-		return token_new(T_LET, value, loc);
-	} else if (strcmp("const", value) == 0) {
-		return token_new(T_CONST, value, loc);
-	} else if (strcmp("while", value) == 0) {
-		return token_new(T_WHILE, value, loc);
-	} else if (strcmp("syscall", value) == 0) {
-		return token_new(T_SYSCALL, value, loc);
-	} else if (strcmp("func", value) == 0) {
-		return token_new(T_FUNC, value, loc);
-	} else if (strcmp("struct", value) == 0) {
-		return token_new(T_STRUCT, value, loc);
-	}
+  if (strcmp("else", value) == 0) {
+    return token_new(T_ELSE, value, loc);
+  } else if (strcmp("dump", value) == 0) {
+    return token_new(T_DUMP, value, loc);
+  } else if (strcmp("mut", value) == 0) {
+    return token_new(T_MUT, value, loc);
+  } else if (strcmp("let", value) == 0) {
+    return token_new(T_LET, value, loc);
+  } else if (strcmp("const", value) == 0) {
+    return token_new(T_CONST, value, loc);
+  } else if (strcmp("while", value) == 0) {
+    return token_new(T_WHILE, value, loc);
+  } else if (strcmp("syscall", value) == 0) {
+    return token_new(T_SYSCALL, value, loc);
+  } else if (strcmp("func", value) == 0) {
+    return token_new(T_FUNC, value, loc);
+  } else if (strcmp("struct", value) == 0) {
+    return token_new(T_STRUCT, value, loc);
+  } else if (strcmp("return", value) == 0) {
+    return token_new(T_RETURN, value, loc);
+  }
 
-
-	return token_new(T_IDENT, value, loc);
+  return token_new(T_IDENT, value, loc);
 }
 
-token_T* read_number(lexer_T* lexer, location_T* loc) {
-	char* value = calloc(2, sizeof(char));
-	size_t i = 0;
+token_T *read_number(lexer_T *lexer, location_T *loc) {
+  char *value = calloc(2, sizeof(char));
+  size_t i = 0;
 
-	while (isdigit(lexer->c)) {
-		value[i++] = lexer->c;
+  while (isdigit(lexer->c)) {
+    value[i++] = lexer->c;
 
-		value = realloc(value, (i + 1) * sizeof(char));
-		advance(lexer);
-	}
+    value = realloc(value, (i + 1) * sizeof(char));
+    advance(lexer);
+  }
 
-	return token_new(T_INTEGER, value, loc);
-
+  return token_new(T_INTEGER, value, loc);
 }
 
-char peek(lexer_T* lexer) {
-	return lexer->content[lexer->i + 1];
+char peek(lexer_T *lexer) { return lexer->content[lexer->i + 1]; }
+
+void skip_whitespace(lexer_T *lexer) {
+  while (isspace(lexer->c)) {
+    advance(lexer);
+  }
 }
 
-void skip_whitespace(lexer_T* lexer) {
-	while (isspace(lexer->c)) {
-		advance(lexer);
-	}
+void skip_singleline_comment(lexer_T *lexer) {
+  while (lexer->c != '\n') {
+    advance(lexer);
+  }
+  advance(lexer);
 }
 
-void skip_singleline_comment(lexer_T* lexer) {
-	while (lexer->c != '\n') {
-		advance(lexer);
-	}
-	advance(lexer);
+void skip_multiline_comment(lexer_T *lexer) {
+  while (!(lexer->c == '*' && peek(lexer) == '/')) {
+    advance(lexer);
+  }
+  advance(lexer);
+  advance(lexer);
 }
 
-void skip_multiline_comment(lexer_T* lexer) {
-	while (!(lexer->c == '*' && peek(lexer) == '/')) {
-		advance(lexer);
-	}
-	advance(lexer);
-	advance(lexer);
+token_T *read_if(lexer_T *lexer, location_T *loc) {
+  char *str = calloc(3, sizeof(char));
+  for (size_t i = 0; i < 2; i++) {
+    str[i] = lexer->c;
+    advance(lexer);
+  }
+
+  return token_new(T_IF, str, loc);
 }
 
-token_T* read_if(lexer_T* lexer, location_T* loc) {
-	char* str = calloc(3, sizeof(char));
-	for (size_t i = 0; i < 2; i++) {
-		str[i] = lexer->c;
-		advance(lexer);
-	}
+token_T *read_pointer(lexer_T *lexer, location_T *loc) {
+  advance(lexer);
+  char *value = calloc(1, sizeof(char));
+  size_t i = 0;
 
-	return token_new(T_IF, str, loc);
+  while (isalnum(lexer->c) || lexer->c == '_') {
+    value[i++] = lexer->c;
+
+    value = realloc(value, (i + 1) * sizeof(char));
+    advance(lexer);
+  }
+
+  return token_new(T_POINTER, value, loc);
 }
 
-token_T* read_pointer(lexer_T* lexer, location_T* loc) {
-	advance(lexer);
-	char* value = calloc(1, sizeof(char));
-	size_t i = 0;
+token_T *read_string(lexer_T *lexer, location_T *loc) {
+  advance(lexer);
+  char *value = calloc(1, sizeof(char));
+  size_t i = 0;
 
-	while (isalnum(lexer->c) || lexer->c == '_') {
-		value[i++] = lexer->c;
+  while (lexer->c != '"') {
+    if (lexer->c == '\\') {
+      value[i++] = lexer->c;
 
-		value = realloc(value, (i + 1) * sizeof(char));
-		advance(lexer);
-	}
+      value = realloc(value, (i + 1) * sizeof(char));
+      advance(lexer);
+    }
 
-	return token_new(T_POINTER, value, loc);
+    value[i++] = lexer->c;
+
+    value = realloc(value, (i + 1) * sizeof(char));
+    advance(lexer);
+  }
+  advance(lexer);
+
+  return token_new(T_STRING, value, loc);
 }
 
-token_T* read_string(lexer_T* lexer, location_T* loc) {
-	advance(lexer);
-	char* value = calloc(1, sizeof(char));
-	size_t i = 0;
+token_T *read_char(lexer_T *lexer, location_T *loc) {
+  advance(lexer);
+  char *value = calloc(1, sizeof(char));
+  size_t i = 0;
+  if (lexer->c == '\\') {
+    value[i++] = lexer->c;
+    value = realloc(value, (i + 1) * sizeof(char));
+    advance(lexer);
+  }
 
-	while (lexer->c != '"') {
-		if (lexer->c == '\\') {
-			value[i++] = lexer->c;
+  value[i++] = lexer->c;
+  value = realloc(value, (i + 1) * sizeof(char));
+  advance(lexer);
 
-			value = realloc(value, (i + 1) * sizeof(char));
-			advance(lexer);
-		}
+  if (lexer->c != '\'') {
+    log_error(loc, 1,
+              "Chars cannot be multiple bytes. Use double quotes if you wish "
+              "to have a string.\n");
+  }
 
-		value[i++] = lexer->c;
+  advance(lexer);
 
-		value = realloc(value, (i + 1) * sizeof(char));
-		advance(lexer);
-	}
-	advance(lexer);
-
-	return token_new(T_STRING, value, loc);
+  return token_new(T_CHAR, value, loc);
 }
 
-token_T* read_char(lexer_T* lexer, location_T* loc) {
-	advance(lexer);
-	char* value = calloc(1, sizeof(char));
-	size_t i = 0;
-	if (lexer->c == '\\') {
-		value[i++] = lexer->c;
-		value = realloc(value, (i + 1) * sizeof(char));
-		advance(lexer);
-	}
-
-	value[i++] = lexer->c;
-	value = realloc(value, (i + 1) * sizeof(char));
-	advance(lexer);
-
-	if (lexer->c != '\'') {
-		log_error(loc, 1, "Chars cannot be multiple bytes. Use double quotes if you wish to have a string.\n");
-	}
-
-	advance(lexer);
-
-	return token_new(T_CHAR, value, loc);
+token_T *advance_with_token(lexer_T *lexer, token_E type, location_T *loc) {
+  char *val = calloc(2, sizeof(char));
+  val[0] = lexer->c;
+  token_T *t = token_new(type, val, loc);
+  advance(lexer);
+  return t;
 }
 
-token_T* advance_with_token(lexer_T* lexer, token_E type, location_T* loc) {
-	char* val = calloc(2, sizeof(char));
-	val[0] = lexer->c;
-	token_T* t = token_new(type, val, loc);
-	advance(lexer);
-	return t;
+token_T *lexer_next_token(lexer_T *lexer) {
+  while (lexer->c != '\0' && lexer->c != -1) {
+    if (isspace(lexer->c)) {
+      skip_whitespace(lexer);
+      continue;
+    }
+
+    location_T *loc = malloc(sizeof(location_T));
+    memcpy(loc, lexer->loc, sizeof(location_T));
+
+    if (lexer->c == '"') {
+      return read_string(lexer, loc);
+    }
+
+    if (lexer->c == '&' && peek(lexer) != '&') {
+      return read_pointer(lexer, loc);
+    } else if (lexer->c == '&') {
+      advance(lexer);
+      return advance_with_token(lexer, T_AND, loc);
+    }
+
+    if (isalpha(lexer->c) || lexer->c == '_') {
+      if (lexer->c == 'i' && peek(lexer) == 'f') {
+        return read_if(lexer, loc);
+      }
+
+      return read_ident(lexer, loc);
+    }
+
+    if (isdigit(lexer->c)) {
+      return read_number(lexer, loc);
+    }
+
+    switch (lexer->c) {
+    case '\'':
+      return read_char(lexer, loc);
+
+    case ':':
+      return advance_with_token(lexer, T_COLON, loc);
+
+    case ';':
+      return advance_with_token(lexer, T_SEMI, loc);
+
+    case ',':
+      return advance_with_token(lexer, T_COMMA, loc);
+
+    case '.':
+      return advance_with_token(lexer, T_DOT, loc);
+
+    case '+':
+      return advance_with_token(lexer, T_PLUS, loc);
+
+    case '-':
+      return advance_with_token(lexer, T_MINUS, loc);
+
+    case '*':
+      return advance_with_token(lexer, T_MULTIPLY, loc);
+
+    case '!': {
+      char next = peek(lexer);
+      switch (next) {
+      case '=': {
+        char *val = calloc(3, sizeof(char));
+        val[0] = lexer->c;
+        advance(lexer);
+        val[1] = lexer->c;
+        advance(lexer);
+        return token_new(T_NOT_EQUALS, val, loc);
+      }
+      }
+      break;
+    }
+
+    case '/': {
+      char next = peek(lexer);
+      switch (next) {
+      case '/':
+        skip_singleline_comment(lexer);
+        break;
+      case '*':
+        skip_multiline_comment(lexer);
+        break;
+      default:
+        return advance_with_token(lexer, T_DIVIDE, loc);
+      }
+      break;
+    }
+
+    case '%':
+      return advance_with_token(lexer, T_MODULUS, loc);
+
+    case '[':
+      return advance_with_token(lexer, T_LSQUARE, loc);
+
+    case ']':
+      return advance_with_token(lexer, T_RSQUARE, loc);
+
+    case '{':
+      return advance_with_token(lexer, T_LCURLY, loc);
+
+    case '}':
+      return advance_with_token(lexer, T_RCURLY, loc);
+
+    case '(':
+      return advance_with_token(lexer, T_LPAREN, loc);
+
+    case ')':
+      return advance_with_token(lexer, T_RPAREN, loc);
+
+    case '=': {
+      if (peek(lexer) == '=') {
+        advance(lexer);
+        advance(lexer);
+        return token_new(T_EQUALS, "==", loc);
+      } else {
+        return advance_with_token(lexer, T_ASSIGN, loc);
+      }
+    }
+
+    case '<':
+      return advance_with_token(lexer, T_LESS, loc);
+
+    case '>':
+      return advance_with_token(lexer, T_GREATER, loc);
+
+    case '|': {
+      if (peek(lexer) == '|') {
+        advance(lexer);
+        return advance_with_token(lexer, T_OR, loc);
+      }
+    }
+
+    default:
+      log_warning("Unexpected character when lexing: '%c' '%s'.\n", lexer->c,
+                  lexer->c);
+      return token_new(T_EOF, "EOF", loc);
+    }
+  }
+
+  return token_new(T_EOF, "EOF", lexer->loc);
 }
-
-
-token_T* lexer_next_token(lexer_T* lexer) {
-	while (lexer->c != '\0' && lexer->c != -1) {
-		if (isspace(lexer->c)) {
-			skip_whitespace(lexer);
-			continue;
-		}
-
-		location_T* loc = malloc(sizeof(location_T));
-		memcpy(loc, lexer->loc, sizeof(location_T));
-
-		if (lexer->c == '"') {
-			return read_string(lexer, loc);
-		}
-
-		if (lexer->c == '&' && peek(lexer) != '&') {
-			return read_pointer(lexer, loc);
-		} else if (lexer->c == '&') {
-			advance(lexer);
-			return advance_with_token(lexer, T_AND, loc);
-		}
-
-		if (isalpha(lexer->c) || lexer->c == '_') {
-			if (lexer->c == 'i' && peek(lexer) == 'f') {
-				return read_if(lexer, loc);
-			}
-
-			return read_ident(lexer, loc);
-		}
-		
-		if (isdigit(lexer->c)) {
-			return read_number(lexer, loc);
-		}
-
-		switch (lexer->c) {
-			case '\'':
-				return read_char(lexer, loc);
-
-			case ':':
-				return advance_with_token(lexer, T_COLON, loc);
-
-			case ';':
-				return advance_with_token(lexer, T_SEMI, loc);
-
-			case ',':
-				return advance_with_token(lexer, T_COMMA, loc);
-
-			case '.':
-				return advance_with_token(lexer, T_DOT, loc);
-
-			case '+':
-				return advance_with_token(lexer, T_PLUS, loc);
-
-			case '-':
-				return advance_with_token(lexer, T_MINUS, loc);
-
-			case '*':
-				return advance_with_token(lexer, T_MULTIPLY, loc);
-
-			case '!':
-				{
-					char next = peek(lexer);
-					switch (next) {
-						case '=':
-							{
-								char* val = calloc(3, sizeof(char));
-								val[0] = lexer->c;
-								advance(lexer);
-								val[1] = lexer->c;
-								advance(lexer);
-								return token_new(T_NOT_EQUALS, val, loc);
-							}
-					}
-					break;
-				}
-
-			case '/':
-				{
-					char next = peek(lexer);
-					switch (next) {
-						case '/':
-							skip_singleline_comment(lexer);
-							break;
-						case '*':
-							skip_multiline_comment(lexer);
-							break;
-						default:
-							return advance_with_token(lexer, T_DIVIDE, loc);
-					}
-					break;
-				}
-
-			case '%':
-				return advance_with_token(lexer, T_MODULUS, loc);
-
-			case '[':
-				return advance_with_token(lexer, T_LSQUARE, loc);
-
-			case ']':
-				return advance_with_token(lexer, T_RSQUARE, loc);
-
-			case '{':
-				return advance_with_token(lexer, T_LCURLY, loc);
-
-			case '}':
-				return advance_with_token(lexer, T_RCURLY, loc);
-
-			case '(':
-				return advance_with_token(lexer, T_LPAREN, loc);
-
-			case ')':
-				return advance_with_token(lexer, T_RPAREN, loc);
-
-			case '=':
-				{
-					if (peek(lexer) == '=') {
-						advance(lexer);
-						advance(lexer);
-						return token_new(T_EQUALS, "==", loc);
-					} else {
-						return advance_with_token(lexer, T_ASSIGN, loc);
-					}
-				}
-
-			case '<':
-				return advance_with_token(lexer, T_LESS, loc);
-
-			case '>':
-				return advance_with_token(lexer, T_GREATER, loc);
-
-			case '|':
-				{
-					if (peek(lexer) == '|') {
-						advance(lexer);
-						return advance_with_token(lexer, T_OR, loc);
-					}
-				}
-		
-			default:
-				log_warning("Unexpected character when lexing: '%c' '%s'.\n", lexer->c, lexer->c);
-				return token_new(T_EOF, "EOF", loc);
-		}
-	}
-
-	return token_new(T_EOF, "EOF", lexer->loc);
-}
-
