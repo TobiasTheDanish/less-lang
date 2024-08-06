ecb71d3da3a73266f67ef22a7a03ee30fb31e71b
THCSC <tobias@skancode.dk>
2024-08-06 23:21:56 +0200

    add var_args to file_utils for more flexible appending to files

--- src/file_util.c
+++ src/file_util.c
@@ -1,64 +1,96 @@
 #include "include/file_util.h"
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-file_T* open_file(char* filePath) {
-	FILE* f = fopen(filePath, "a+");
-	if (f == NULL) {
-		perror("File not found");
-		exit(1);
-	}
+file_T *open_file(char *filePath) {
+  FILE *f = fopen(filePath, "a+");
+  if (f == NULL) {
+    perror("File not found");
+    exit(1);
+  }
 
-	file_T* file = malloc(sizeof(file_T));
-	file->f = f;
-	file->filePath = filePath;
-	file->modes = "a+";
+  file_T *file = malloc(sizeof(file_T));
+  file->f = f;
+  file->filePath = filePath;
+  file->modes = "a+";
 
-	return file;
+  return file;
 }
 
-file_T* open_file_m(char* filePath, char* modes) {
-	FILE* f = fopen(filePath, modes);
-	if (f == NULL) {
-		perror("File not found");
-		exit(1);
-	}
+file_T *open_file_m(char *filePath, char *modes) {
+  FILE *f = fopen(filePath, modes);
+  if (f == NULL) {
+    perror("File not found");
+    exit(1);
+  }
 
-	file_T* file = malloc(sizeof(file_T));
-	file->f = f;
-	file->filePath = filePath;
-	file->modes = "a+";
+  file_T *file = malloc(sizeof(file_T));
+  file->f = f;
+  file->filePath = filePath;
+  file->modes = "a+";
 
-	return file;
+  return file;
 }
 
-void close_file(file_T *file) {
-	fclose(file->f);
+void close_file(file_T *file) { fclose(file->f); }
+
+char *read_file(file_T *file) {
+  size_t capacity = 100;
+  char *contents = calloc(capacity, sizeof(char));
+  size_t size = 0;
+
+  while (!feof(file->f)) {
+    contents[size] = fgetc(file->f);
+    size += 1;
+
+    if (size == capacity) {
+      capacity *= 1.5;
+
+      contents = realloc(contents, capacity * sizeof(char));
+    }
+  }
+
+  fclose(file->f);
+
+  return contents;
 }
 
-char* read_file(file_T* file) {
-	size_t capacity = 100;
-	char* contents = calloc(capacity, sizeof(char));
-	size_t size = 0;
+void append_file(file_T *file, char *format, ...) {
+  va_list args;
+  va_start(args, format);
 
-	while (!feof(file->f)) 
-	{
-		contents[size] = fgetc(file->f);
-		size += 1;
+  while (*format) {
+    if (*format == '%') {
+      format++; // Move past '%'
+      if (*format == '\0')
+        break; // Format error
 
-		if (size == capacity) 
-		{
-			capacity *= 1.5;
+      if (*format == 'd' || *format == 'c') {
+        int num = va_arg(args, int);
+        fprintf(file->f, "%d", num);
+      } else if (*format == 's') {
+        char *str = va_arg(args, char *);
+        fprintf(file->f, "%s", str);
+      } else if (*format == 'f') {
+        double num = va_arg(args, double);
+        fprintf(file->f, "%f", num);
+      } else if (*format == 'u') {
+        unsigned int num = va_arg(args, unsigned int);
+        fprintf(file->f, "%u", num);
+      } else if (*format == 'p') {
+        void *ptr = va_arg(args, void *);
+        fprintf(file->f, "%p", ptr);
+      } else {
+        fputc('%', file->f);
+        fputc(*format, file->f);
+      }
 
-			contents = realloc(contents, capacity * sizeof(char));
-		}
-	}
+    } else {
+      fputc(*format, file->f);
+    }
+    format++;
+  }
 
-	fclose(file->f);
-
-	return contents;
-}
-
-void append_file(file_T* file, char* content) {
-	fprintf(file->f, "%s", content);
+  va_end(args);
 }
--- src/include/file_util.h
+++ src/include/file_util.h
@@ -4,20 +4,18 @@
 #include <stdio.h>
 
 typedef struct FILE_STRUCT {
-	FILE* f;
-	char* filePath;
-	char* modes;
+  FILE *f;
+  char *filePath;
+  char *modes;
 } file_T;
 
-file_T* open_file(char* filePath);
-file_T* open_file_m(char* filePath, char* modes);
+file_T *open_file(char *filePath);
+file_T *open_file_m(char *filePath, char *modes);
 
-void close_file(file_T* file);
+void close_file(file_T *file);
 
-char* read_file(file_T* file);
+char *read_file(file_T *file);
 
-void append_file(file_T* file, char* content);
+void append_file(file_T *file, char *content, ...);
 
 #endif // !FILE_READER_H
-
-
